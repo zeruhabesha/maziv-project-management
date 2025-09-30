ca0386a5d61f893fcfa4738129d43d34ea730fdc
Zerihun Kibret <88440051+zeruhabesha@users.noreply.github.com>
2025-09-30 09:54:36 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -10,14 +10,21 @@ export default {
   },
   test: { /* same as above */ },
   production: {
-    username: "maziv_user",
-    password: "Y0x1lLB1r8AI8oLmOQ009R3ej0eVY4I7",
-    database: "maziv_project",
-    host: "dpg-d2neh77diees73cicfl0-a.oregon-postgres.render.com",
-    port: 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false }
-    }
-  }
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false }
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  logging: false
+}
 };
